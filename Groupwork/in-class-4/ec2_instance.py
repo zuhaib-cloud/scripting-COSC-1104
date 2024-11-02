b302be2c6d87a159999f91f85020191b06c7d23b
Zuhaib_khan <mdzuhaibkhan1999@gmail.com>
2024-11-02 08:38:39 -0400

    added json file

---
+++ Groupwork/in-class-4/ec2_instance.py
@@ -0,0 +1,8 @@
+import requests
+data=requests.get(url="C:\Durham\Cloud\repositories\scripting-COSC-1104\Groupwork\in-class-4\ec2_instance_types.json")
+#data.json()
+
+#def retrieve_instance_data()
+#with open('ec2_instance_types.json', 'r') as file:
+#data = json.load(file)
+print(data)
